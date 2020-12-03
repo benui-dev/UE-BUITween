57c38280633253b1030a40634dd55f2710355581
ben <32515+benui-dev@users.noreply.github.com>
2020-12-03 12:25:22 -0800

    Moved functions out of header

--- Source/UITween/Private/BUITween.cpp
+++ Source/UITween/Private/BUITween.cpp
@@ -1,5 +1,12 @@
 #include "BUITween.h"
 
+#include "Components/Widget.h"
+#include "Components/Image.h"
+#include "Components/Border.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Components/Sizebox.h"
+#include "Blueprint/UserWidget.h"
+
 TArray< FBUITweenInstance > UBUITween::ActiveInstances = TArray< FBUITweenInstance >();
 TArray< FBUITweenInstance > UBUITween::InstancesToAdd = TArray< FBUITweenInstance >();
 bool UBUITween::bIsInitialized = false;
@@ -51,14 +58,13 @@ int32 UBUITween::Clear( UWidget* pInWidget )
 }
 
 
-
-void UBUITween::Update( float InDeltaTime )
+void UBUITween::Update( float DeltaTime )
 {
 	// Reverse it so we can remove
 	for ( int32 i = ActiveInstances.Num()-1; i >= 0; --i )
 	{
 		FBUITweenInstance& Inst = ActiveInstances[ i ];
-		Inst.Update( InDeltaTime  );
+		Inst.Update( DeltaTime );
 		if ( Inst.IsComplete() )
 		{
 			FBUITweenInstance CompleteInst = Inst;
@@ -90,3 +96,159 @@ bool UBUITween::GetIsTweening( UWidget* pInWidget )
 	return false;
 }
 
+void FBUITweenInstance::Begin()
+{
+	bShouldUpdate = true;
+	bHasPlayedStartEvent = false;
+	bHasPlayedCompleteEvent = false;
+
+	if ( !pWidget.IsValid() )
+	{
+		UE_LOG( LogTemp, Warning, TEXT( "Trying to start invalid widget" ) );
+		return;
+	}
+
+	// Set all the props to the existng state
+	TranslationProp.OnBegin( pWidget->RenderTransform.Translation );
+	ScaleProp.OnBegin( pWidget->RenderTransform.Scale );
+	OpacityProp.OnBegin( pWidget->GetRenderOpacity() );
+
+	{
+		UUserWidget* UW = Cast<UUserWidget>( pWidget );
+		if ( UW )
+		{
+			ColorProp.OnBegin( UW->ColorAndOpacity );
+		}
+		UImage* UI = Cast<UImage>( pWidget );
+		if ( UI )
+		{
+			ColorProp.OnBegin( UI->ColorAndOpacity );
+		}
+		UBorder* Border = Cast<UBorder>( pWidget );
+		if ( Border )
+		{
+			ColorProp.OnBegin( Border->ContentColorAndOpacity );
+		}
+	}
+
+	// Apply the starting conditions, even if we delay
+	Apply( 0 );
+}
+
+void FBUITweenInstance::Update( float DeltaTime )
+{
+	if ( !bShouldUpdate && !bIsComplete )
+	{
+		return;
+	}
+	if ( !pWidget.IsValid() )
+	{
+		bIsComplete = true;
+		return;
+	}
+
+	if ( Delay > 0 )
+	{
+		// TODO could correctly subtract from deltatime and use rmaining on alpha but meh
+		Delay -= DeltaTime;
+		return;
+	}
+
+	if ( !bHasPlayedStartEvent )
+	{
+		OnStartedDelegate.ExecuteIfBound( pWidget.Get() );
+		bHasPlayedStartEvent = true;
+	}
+
+	// Tween each thingy
+	Alpha += DeltaTime;
+	if ( Alpha >= Duration )
+	{
+		Alpha = Duration;
+		bIsComplete = true;
+	}
+
+	const float EasedAlpha = EasingParam.IsSet()
+		? FBUIEasing::Ease( EasingType, Alpha, Duration, EasingParam.GetValue() )
+		: FBUIEasing::Ease( EasingType, Alpha, Duration );
+
+	Apply( EasedAlpha );
+}
+
+void FBUITweenInstance::Apply( float EasedAlpha )
+{
+	UWidget* Target = pWidget.Get();
+
+	if ( ColorProp.IsSet() )
+	{
+		ColorProp.Update( EasedAlpha );
+		UUserWidget* UW = Cast<UUserWidget>( Target );
+		if ( UW )
+		{
+			UW->SetColorAndOpacity( ColorProp.CurrentValue );
+		}
+		UImage* UI = Cast<UImage>( Target );
+		if ( UI )
+		{
+			UI->SetColorAndOpacity( ColorProp.CurrentValue );
+		}
+		UBorder* Border = Cast<UBorder>( Target );
+		if ( Border )
+		{
+			Border->SetContentColorAndOpacity( ColorProp.CurrentValue );
+		}
+	}
+
+	if ( OpacityProp.IsSet() )
+	{
+		OpacityProp.Update( EasedAlpha );
+		Target->SetRenderOpacity( OpacityProp.CurrentValue );
+	}
+
+	// Only apply visibility changes at 0 or 1
+	if ( VisibilityProp.IsSet() )
+	{
+		if ( VisibilityProp.Update( EasedAlpha ) )
+		{
+			Target->SetVisibility( VisibilityProp.CurrentValue );
+		}
+	}
+
+	bool bChangedRenderTransform = false;
+	FWidgetTransform CurrentTransform = Target->RenderTransform;
+
+	if ( TranslationProp.IsSet() )
+	{
+		TranslationProp.Update( EasedAlpha );
+		CurrentTransform.Translation = TranslationProp.CurrentValue;
+		bChangedRenderTransform = true;
+	}
+	if ( ScaleProp.IsSet() )
+	{
+		ScaleProp.Update( EasedAlpha );
+		CurrentTransform.Scale = ScaleProp.CurrentValue;
+		bChangedRenderTransform = true;
+	}
+	if ( RotationProp.IsSet() )
+	{
+		if ( RotationProp.Update( EasedAlpha ) )
+		{
+			CurrentTransform.Angle = RotationProp.CurrentValue;
+			bChangedRenderTransform = true;
+		}
+	}
+	if ( CanvasPositionProp.IsSet() )
+	{
+		if ( CanvasPositionProp.Update( EasedAlpha ) )
+		{
+			UCanvasPanelSlot* CanvasSlot = Cast<UCanvasPanelSlot>( pWidget->Slot );
+			if ( CanvasSlot )
+				CanvasSlot->SetPosition( CanvasPositionProp.CurrentValue );
+		}
+	}
+
+	if ( bChangedRenderTransform )
+	{
+		Target->SetRenderTransform( CurrentTransform );
+	}
+}
--- Source/UITween/Public/BUITween.h
+++ Source/UITween/Public/BUITween.h
@@ -1,12 +1,7 @@
 #pragma once
 
-#include "Components/Widget.h"
-#include "Components/Image.h"
-#include "Components/Border.h"
-#include "Components/CanvasPanelSlot.h"
-#include "Components/Sizebox.h"
-#include "Blueprint/UserWidget.h"
 #include "../Private/BUIEasing.h"
+#include "Components/Widget.h"
 #include "BUITween.generated.h"
 
 DECLARE_DELEGATE_OneParam( FBUITweenSignature, UWidget* /*Owner*/ );
@@ -100,7 +95,7 @@ public:
 
 
 USTRUCT()
-struct FBUITweenInstance
+struct BUITWEEN_API FBUITweenInstance
 {
 	GENERATED_BODY()
 
@@ -113,165 +108,15 @@ public:
 	{
 		ensure( pInWidget != nullptr );
 	}
-	void Begin()
-	{
-		bShouldUpdate = true;
-		bHasPlayedStartEvent = false;
-		bHasPlayedCompleteEvent = false;
+	void Begin();
+	void Update( float InDeltaTime );
+	void Apply( float EasedAlpha );
 
-		if ( !pWidget.IsValid() )
-		{
-			UE_LOG( LogTemp, Warning, TEXT( "Trying to start invalid widget" ) );
-			return;
-		}
-
-		// Set all the props to the existng state
-		TranslationProp.OnBegin( pWidget->RenderTransform.Translation );
-		ScaleProp.OnBegin( pWidget->RenderTransform.Scale );
-		OpacityProp.OnBegin( pWidget->GetRenderOpacity() );
-
-		{
-			UUserWidget* UW = Cast<UUserWidget>( pWidget );
-			if ( UW )
-			{
-				ColorProp.OnBegin( UW->ColorAndOpacity );
-			}
-			UImage* UI = Cast<UImage>( pWidget );
-			if ( UI )
-			{
-				ColorProp.OnBegin( UI->ColorAndOpacity );
-			}
-			UBorder* Border = Cast<UBorder>( pWidget );
-			if ( Border )
-			{
-				ColorProp.OnBegin( Border->ContentColorAndOpacity );
-			}
-		}
-
-		// Apply the starting conditions, even if we delay
-		Apply( 0 );
-	}
-	void Update( float InDeltaTime )
-	{
-		if ( !bShouldUpdate && !bIsComplete )
-		{
-			return;
-		}
-		if ( !pWidget.IsValid() )
-		{
-			bIsComplete = true;
-			return;
-		}
-
-		if ( Delay > 0 )
-		{
-			// TODO could correctly subtract from deltatime and use rmaining on alpha but meh
-			Delay -= InDeltaTime;
-			return;
-		}
-
-		if ( !bHasPlayedStartEvent )
-		{
-			OnStartedDelegate.ExecuteIfBound( pWidget.Get() );
-			bHasPlayedStartEvent = true;
-		}
-
-		// Tween each thingy
-		Alpha += InDeltaTime;
-		if ( Alpha >= Duration )
-		{
-			Alpha = Duration;
-			bIsComplete = true;
-		}
-
-		const float EasedAlpha = EasingParam.IsSet()
-			? FBUIEasing::Ease( EasingType, Alpha, Duration, EasingParam.GetValue() )
-			: FBUIEasing::Ease( EasingType, Alpha, Duration );
-
-		Apply( EasedAlpha );
-	}
-	void Apply( float EasedAlpha )
-	{
-		UWidget* Target = pWidget.Get();
-
-		if ( ColorProp.IsSet() )
-		{
-			ColorProp.Update( EasedAlpha );
-			UUserWidget* UW = Cast<UUserWidget>( Target );
-			if ( UW )
-			{
-				UW->SetColorAndOpacity( ColorProp.CurrentValue );
-			}
-			UImage* UI = Cast<UImage>( Target );
-			if ( UI )
-			{
-				UI->SetColorAndOpacity( ColorProp.CurrentValue );
-			}
-			UBorder* Border = Cast<UBorder>( Target );
-			if ( Border )
-			{
-				Border->SetContentColorAndOpacity( ColorProp.CurrentValue );
-			}
-		}
-
-		if ( OpacityProp.IsSet() )
-		{
-			OpacityProp.Update( EasedAlpha );
-			Target->SetRenderOpacity( OpacityProp.CurrentValue );
-		}
-
-		// Only apply visibility changes at 0 or 1
-		if ( VisibilityProp.IsSet() )
-		{
-			if ( VisibilityProp.Update( EasedAlpha ) )
-			{
-				Target->SetVisibility( VisibilityProp.CurrentValue );
-			}
-		}
-
-		bool bChangedRenderTransform = false;
-		FWidgetTransform CurrentTransform = Target->RenderTransform;
-
-		if ( TranslationProp.IsSet() )
-		{
-			TranslationProp.Update( EasedAlpha );
-			CurrentTransform.Translation = TranslationProp.CurrentValue;
-			bChangedRenderTransform = true;
-		}
-		if ( ScaleProp.IsSet() )
-		{
-			ScaleProp.Update( EasedAlpha );
-			CurrentTransform.Scale = ScaleProp.CurrentValue;
-			bChangedRenderTransform = true;
-		}
-		if ( RotationProp.IsSet() )
-		{
-			if ( RotationProp.Update( EasedAlpha ) )
-			{
-				CurrentTransform.Angle = RotationProp.CurrentValue;
-				bChangedRenderTransform = true;
-			}
-		}
-		if ( CanvasPositionProp.IsSet() )
-		{
-			if ( CanvasPositionProp.Update( EasedAlpha ) )
-			{
-				UCanvasPanelSlot* CanvasSlot = Cast<UCanvasPanelSlot>( pWidget->Slot );
-				if ( CanvasSlot )
-					CanvasSlot->SetPosition( CanvasPositionProp.CurrentValue );
-			}
-		}
-
-		if ( bChangedRenderTransform )
-		{
-			Target->SetRenderTransform( CurrentTransform );
-		}
-	}
 	inline bool operator==( const FBUITweenInstance& other) const
 	{
 		return pWidget == other.pWidget;
 	}
-	FORCEINLINE bool IsComplete() const { return bIsComplete; }
+	bool IsComplete() const { return bIsComplete; }
 
 	// EasingParam is used for easing functions that have a second parameter, like Elastic
 	FBUITweenInstance& Easing( EBUIEasingType InType, TOptional<float> InEasingParam = TOptional<float>() )
